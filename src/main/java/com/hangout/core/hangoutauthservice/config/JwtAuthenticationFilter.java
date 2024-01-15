1094b7cb5da18823e55bf6ea96d6e042129ed882
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2024-01-16 02:17:50 +0530

    The issue with securityconfig is fixed.
    CSRF mismatch was the main problem, so I disabled it. Also disabled CORS for testing purpose

--- src/main/java/com/hangout/core/hangoutauthservice/config/JwtAuthenticationFilter.java
+++ src/main/java/com/hangout/core/hangoutauthservice/config/JwtAuthenticationFilter.java
@@ -1,7 +1,12 @@
 package com.hangout.core.hangoutauthservice.config;
 
-import java.io.IOException;
-
+import com.hangout.core.hangoutauthservice.service.JwtService;
+import io.micrometer.common.lang.NonNull;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,47 +15,38 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import com.hangout.core.hangoutauthservice.service.JwtService;
-
-import io.micrometer.common.lang.NonNull;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
+import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-	private final JwtService jwtService;
-	private final UserDetailsService userDetailsService;
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
 
-	@Override
-	protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
-			@NonNull FilterChain filterChain) throws ServletException, IOException {
-		final String authHeader = request.getHeader("Authorization");
-		final String jwt;
-		final String userEmail;
-		// for public urls
-		if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-			filterChain.doFilter(request, response);
-			return;
-		}
-		// To cut out the "Bearer " part
-		jwt = authHeader.substring(7);
-		userEmail = jwtService.extractUsername(jwt, true);
-		if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-			// user present but not authenticated yet
-			UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
-			if (jwtService.isTokenValid(jwt, userDetails, true)) {
-				UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails,
-						null, userDetails.getAuthorities());
-				authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-				SecurityContextHolder.getContext().setAuthentication(authToken);
-			}
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+        // for public urls
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        // To cut out the "Bearer " part
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt, true);
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            // user present but not authenticated yet
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+            if (jwtService.isTokenValid(jwt, userDetails, true)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
 
-		}
-		filterChain.doFilter(request, response);
-	}
+        }
+        filterChain.doFilter(request, response);
+    }
 
 }
