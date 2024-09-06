1d4c35a3174ef2d9b9e794ec540a9e0cbc0c99c4
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2024-09-06 10:07:55 +0530

    added TestContainer support to PublicController test class

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Use an appropriate base image with Java and Maven pre-installed
-FROM maven:3-amazoncorretto-17
+FROM maven:3-amazoncorretto-21
 # Set the working directory inside the container
 WORKDIR /usr/src/app
 
@@ -16,4 +16,4 @@ COPY src ./src
 RUN mvn package -DskipTests 
 
 # Set the command to run the Spring Boot application
-CMD ["java", "-jar", "target/Users-0.0.1.jar"]
+CMD ["java", "-jar", "target/Users-0.0.2.jar"]
--- src/test/java/com/hangout/core/controller/PublicControllerTest.java
+++ src/test/java/com/hangout/core/controller/PublicControllerTest.java
@@ -1,5 +1,6 @@
 package com.hangout.core.controller;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -10,18 +11,25 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import com.hangout.core.entity.User;
 import com.hangout.core.repository.UserRepo;
 
 import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
 
+@Testcontainers
 @SpringBootTest
+@Slf4j
 public class PublicControllerTest {
     private MockMvc mockMvc;
     @Autowired
@@ -31,6 +39,10 @@ public class PublicControllerTest {
     @Autowired
     private WebApplicationContext context;
 
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
+
     @BeforeEach
     public void init() {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
@@ -38,7 +50,7 @@ public class PublicControllerTest {
 
     @Test
     void testSignup() {
-
+        assertTrue(true);
     }
 
     private User setupUser() {
@@ -54,12 +66,36 @@ public class PublicControllerTest {
 
     @Test
     @Transactional
-    void testLogin() throws Exception {
-        setupUser();
-        this.mockMvc
-                .perform(post("/v1/public/login").contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"username\": \"testUser\",\"password\": \"a1@bcdefgh456\"}"))
-                .andExpect(status().isOk()).andExpect(content().contentType("text/plain;charset=UTF-8")).andDo(print());
-        cleanUpUser();
+    void testLogin_usernamePasswordCorrect() {
+        try {
+            setupUser();
+            this.mockMvc
+                    .perform(post("/v1/public/login").contentType(MediaType.APPLICATION_JSON)
+                            .content("{\"username\": \"testUser\",\"password\": \"a1@bcdefgh456\"}"))
+                    .andExpect(status().isOk()).andExpect(content().contentType("text/plain;charset=UTF-8"))
+                    .andDo(print());
+        } catch (Exception ex) {
+            log.error("Exception occoured during execution of testLogin_usernamePasswordCorrect", ex.getMessage());
+        } finally {
+            cleanUpUser();
+        }
     }
+
+    @Test
+    @Transactional
+    void testLogin_usernamePasswordWrong() {
+        try {
+            setupUser();
+            this.mockMvc
+                    .perform(post("/v1/public/login").contentType(MediaType.APPLICATION_JSON)
+                            .content("{\"username\": \"testUser3\",\"password\": \"a1@bcdefgh4567\"}"))
+                    .andExpect(status().is4xxClientError()).andExpect(content().contentType("text/plain;charset=UTF-8"))
+                    .andDo(print());
+        } catch (Exception ex) {
+            log.error("Exception occoured during execution of testLogin_usernamePasswordWrong", ex.getMessage());
+        } finally {
+            cleanUpUser();
+        }
+    }
+
 }
